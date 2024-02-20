a40e95efb32704ffea8fae4c9547124028b93b49
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-20 22:10:14 +0800

    Mahabub

---
+++ problem_solving31.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char c[1000001];
+    fgets(c,1000001,stdin);
+    for(int i=0;c[i]!='\\';i++)
+    {
+        printf("%c",c[i]);
+    }
+}
