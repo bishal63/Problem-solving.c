aa59c41835b237a0b74d13a22765e969fc49369e
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-20 22:21:01 +0800

    Mahabub

---
+++ problem_solving44.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void mahabub()
+{
+    char m;
+    scanf("%c",&m);
+    printf("%d",m);
+}
+int main()
+{
+    mahabub();
+    //char m;
+    //scanf("%c",&m);
+    //printf("%d",mahabub);
+}
