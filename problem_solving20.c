1e2fe6c63525c277e46a87c2b6849bd67cf318c1
Md.Mahabub Alam Bishal <160615442+bishal63@users.noreply.github.com>
2024-02-20 22:00:08 +0800

    Mahabub

---
+++ problem_solving20.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+        //printf("%d",arr[i]);
+        sum=sum+arr[i];     
+    }
+    //printf("%d",sum);
+    if(sum>0)
+    {
+        printf("%d",sum);
+    }
+    else
+    {
+        printf("%d",sum*-1);
+    }
+}
